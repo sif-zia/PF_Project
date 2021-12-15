e3f703b9a096ca7ece8dcce1f30f464581326efc
Sif Zia <sifzia@yahoo.com>
2021-12-15 19:24:11 +0500

    Perfected graphics

--- game.cpp
+++ game.cpp
@@ -20,57 +20,77 @@ void DrawGrid() {
         myLine(50, y, 610, y, 255);
 }
 
+void EmptyCells() {
+    int size = 20;
+
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            int mid_x = (100 + 70 * (2 * i + 1)) / 2;
+            int mid_y = (100 + 70 * (2 * j + 1)) / 2;
+            myRect(mid_x - size, mid_y - size, mid_x + size, mid_y + size, 0, 0, 0);
+        }
+    }
+
+}
+
 void randomizer(int cells[8][8]) {
+    EmptyCells();
     for (int i = 0; i < 8; i++)
         for (int j = 0; j < 8; j++)
             cells[i][j] = (rand() % 5) + 1;
 }
 
-void DrawGems(int cells[8][8], int i, int j, int gem) {
-    int mid_x = (100 + 70 * (2 * i + 1)) / 2;
-    int mid_y = (100 + 70 * (2 * j + 1)) / 2;
+void DrawGems(int cells[8][8]) {
+    int size = 15;
 
-    if(gem == 1)
-        myEllipse(mid_x - 15, mid_y - 15, mid_x + 15, mid_y + 15, 255, 0, 255);
-    else if (gem == 2) {
-        myLine(mid_x, mid_y - 15, mid_x + 15, mid_y + 15, 0, 0, 255);
-        myLine(mid_x + 15, mid_y + 15, mid_x - 15, mid_y + 15, 0, 0, 255);
-        myLine(mid_x - 15, mid_y + 15, mid_x, mid_y - 15, 0, 0, 255);
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            int mid_x = (100 + 70 * (2 * i + 1)) / 2;
+            int mid_y = (100 + 70 * (2 * j + 1)) / 2;
+            if (cells[i][j] == 1)
+                myEllipse(mid_x - size, mid_y - size, mid_x + size, mid_y + size, 255, 0, 255);
+            else if (cells[i][j] == 2) {
+                myLine(mid_x, mid_y - size, mid_x + size, mid_y + size, 0, 0, 255);
+                myLine(mid_x + size, mid_y + size, mid_x - size, mid_y + size, 0, 0, 255);
+                myLine(mid_x - size, mid_y + size, mid_x, mid_y - size, 0, 0, 255);
+            }
+            else if (cells[i][j] == 3)
+                myEllipse(mid_x - size/2, mid_y - size, mid_x + size/2, mid_y + size, 255, 255, 0);
+            else if (cells[i][j] == 4)
+                myRect(mid_x - size, mid_y - size, mid_x + size, mid_y + size, 0, 255, 255);
+            else if (cells[i][j] == 5) {
+                myLine(mid_x - size, mid_y - size, mid_x + size, mid_y + size, 0, 255, 0);
+                myLine(mid_x - size, mid_y + size, mid_x + size, mid_y - size, 0, 255, 0);
+            }
+        }
     }
-    else if(gem == 3)
-        myEllipse(mid_x - 8, mid_y - 15, mid_x + 8, mid_y + 15, 255, 255, 0);
-    else if(gem == 4)
-        myRect(mid_x - 15, mid_y - 15, mid_x + 15, mid_y + 15, 0, 255, 255);
-    else if (gem == 5) {
-        myLine(mid_x - 15, mid_y - 15, mid_x + 15, mid_y + 15, 0, 255, 0);
-        myLine(mid_x - 15, mid_y + 15, mid_x + 15, mid_y - 15, 0, 255, 0);
-    }
+    
 }
 
 void MoveSelectorLeft(int& x, int& y) {
     if (x != 50) {
-        myRect(x, y, x + 70, y + 70, 0, 0, 0);
+        myRect(x, y, x + 70, y + 70, 255, 255, 255);
         x -= 70;
     }
 }
 
 void MoveSelectorRight(int& x, int& y) {
     if (x + 70 != 610) {
-        myRect(x, y, x + 70, y + 70, 0, 0, 0);
+        myRect(x, y, x + 70, y + 70, 255, 255, 255);
         x += 70;
     }
 }
 
 void MoveSelectorUp(int& x, int& y) {
     if (y != 50) {
-        myRect(x, y, x + 70, y + 70, 0, 0, 0);
+        myRect(x, y, x + 70, y + 70, 255, 255, 255);
         y -= 70;
     }
 }
 
 void MoveSelectorDown(int& x, int& y) {
     if (y + 70 != 610) {
-        myRect(x, y, x + 70, y + 70, 0, 0, 0);
+        myRect(x, y, x + 70, y + 70, 255, 255, 255);
         y += 70;
     }
 }
@@ -89,29 +109,48 @@ int main() {
     int cells[8][8] = {};
 
     randomizer(cells);
+    DrawGems(cells);
+    DrawGrid();
+    myRect(x, y, x + 70, y + 70, 255, 0, 0);
+    DrawGems(cells);
 
 
     while (1) {
-        DrawGrid();
-
-        myRect(x, y, x + 70, y + 70, 255, 0, 0);
-        DrawGems(cells, 0, 0, 1);
-        DrawGems(cells, 0, 1, 2);
-        DrawGems(cells, 0, 2, 3);
-        DrawGems(cells, 0, 3, 4);
-        DrawGems(cells, 0, 4, 5);
-        Sleep(50);
 
         key_pressed = isCursorKeyPressed(keyboard_key);
+        
+        
 
-        if (key_pressed == true && keyboard_key == 1)
+        if (key_pressed == true && keyboard_key == 1) {
             MoveSelectorLeft(x, y);
-        else if (key_pressed == true && keyboard_key == 2)
+            DrawGrid();
+            myRect(x, y, x + 70, y + 70, 255, 0, 0);
+            DrawGems(cells);
+        }
+        else if (key_pressed == true && keyboard_key == 2) {
             MoveSelectorUp(x, y);
-        else if (key_pressed == true && keyboard_key == 3)
+            DrawGrid();
+            myRect(x, y, x + 70, y + 70, 255, 0, 0);
+            DrawGems(cells);
+        }
+        else if (key_pressed == true && keyboard_key == 3) {
             MoveSelectorRight(x, y);
-        else if (key_pressed == true && keyboard_key == 4)
+            DrawGrid();
+            myRect(x, y, x + 70, y + 70, 255, 0, 0);
+            DrawGems(cells);
+        }
+        else if (key_pressed == true && keyboard_key == 4) {
             MoveSelectorDown(x, y);
+            DrawGrid();
+            myRect(x, y, x + 70, y + 70, 255, 0, 0);
+            DrawGems(cells);
+        }
+        else if (key_pressed == true && keyboard_key == 5) {
+            randomizer(cells);
+            DrawGrid();
+            myRect(x, y, x + 70, y + 70, 255, 0, 0);
+            DrawGems(cells);
+        }
 
     }
     cin.get();
